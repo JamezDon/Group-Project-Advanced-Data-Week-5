9a9ed3f32b29b346b31dbec6849f1bf20e47a8ae
Najma <trainee.najma.hassan@sigmalabs.co.uk>
2025-06-03 14:59:28 +0100

    refactoring functions and changes to tests

--- pipeline/test_validate.py
+++ pipeline/test_validate.py
@@ -1,61 +1,118 @@
 """Tests for validation functions."""
 
-from extract import
-from validate import (check_missing_keys, check_missing_location_details,
+
+from validate import (get_dict_of_missing_info, check_missing_keys, check_missing_location_details,
                       check_missing_botanist_details)
 
 
-def test_check_missing_keys_returns_list_of_missing_keys_1():
-    """Tests that the function returns a list of missing keys 
+def test_get_dict_of_missing_info_returns_correct_missing_keys():
+    """Tests that the function returns the correct missing keys when 
+    given a list of required keys."""
+
+    required_keys = ["name", "plant_id", "origin_location", "soil_moisture"]
+    data = {"name": "/", "plant_id": "/"}
+    assert get_dict_of_missing_info(data, required_keys) == {"missing_keys": [
+        "origin_location", "soil_moisture"]}
+
+
+def test_get_dict_of_missing_info_returns_correct_missing_values():
+    """Tests that the function returns the correct missing values when 
+    given a list of required keys."""
+
+    required_keys = ["name", "plant_id", "origin_location", "soil_moisture"]
+    data = {"name": "/", "plant_id": "/",
+            "origin_location": "", "soil_moisture": ""}
+    assert get_dict_of_missing_info(data, required_keys) == {"missing_values": [
+        "origin_location", "soil_moisture"]}
+
+
+def test_get_dict_of_missing_info_returns_correct_missing_keys_and_values():
+    """Tests that the function returns the correct missing keys and values when 
+    given a list of required keys."""
+
+    required_keys = ["name", "plant_id", "origin_location", "soil_moisture"]
+    data = {"name": "/", "origin_location": "", "soil_moisture": ""}
+    assert get_dict_of_missing_info(data, required_keys) == {"missing_keys": ["plant_id"], "missing_values": [
+        "origin_location", "soil_moisture"]}
+
+
+def test_get_dict_of_missing_info_returns_empty_dict_when_no_missing_keys_and_values():
+    """Tests that the function returns an empty list when there are no missing keys when
+    given a list of required keys."""
+
+    required_keys = ["name", "plant_id", "origin_location", "soil_moisture"]
+    data = {"name": "/", "plant_id": "/",
+            "origin_location": "/", "soil_moisture": "/"}
+    assert get_dict_of_missing_info(data, required_keys) == {}
+
+
+def test_check_missing_keys_returns_dict_of_missing_keys():
+    """Tests that the function returns a dict of missing keys 
     if last_watered, soil_moisture and recording_taken keys are missing from plant data. """
 
-    data = {"plant_id": 7, "name": "", "temperature": "",
-            "origin_location": "", "botanist": ""}
+    data = {"plant_id": 7, "name": "/", "temperature": "/",
+            "origin_location": "/", "botanist": "/"}
     assert check_missing_keys(
-        data) == ["last_watered", "soil_moisture", "recording_taken"]
+        data) == {"missing_keys": ["last_watered", "soil_moisture", "recording_taken"]}
 
 
-def test_check_missing_keys_returns_list_of_missing_keys_2():
-    """Tests that the function returns a full list of missing keys 
-    if all valid keys are missing from plant data. """
+def test_check_missing_keys_returns_dict_of_missing_values():
+    """Tests that the function returns a dict of missing values 
+    if last_watered, soil_moisture and recording_taken keys have empty values. """
 
-    data = {}
+    data = {"plant_id": 7, "name": "/", "temperature": "/",
+            "origin_location": "/", "botanist": "/", "last_watered": "", "soil_moisture": "",
+            "recording_taken": ""}
     assert check_missing_keys(
-        data) == ["plant_id", "name", "temperature", "origin_location", "botanist",
-                  "last_watered", "soil_moisture", "recording_taken"]
+        data) == {"missing_values": ["last_watered", "soil_moisture", "recording_taken"]}
 
 
-def test_check_missing_keys_returns_empty_list_when_no_missing_keys():
-    """Tests that the function returns an empty list of missing keys 
-    if no top level keys are missing from plant data. """
+def test_check_missing_keys_returns_dict_of_missing_key_and_values():
+    """Tests that the function returns a dict of missing keys and missing values
+    if plant_id and name keys are missing and last_watered, soil_moisture and 
+    recording_taken keys are empty values."""
 
-    data = {"plant_id": "", "name": "", "temperature": "", "origin_location": "", "botanist": "",
-            "last_watered": "", "soil_moisture": "", "recording_taken": ""}
-    assert check_missing_keys(data) == []
-
-
-def test_check_missing_location_details_return_list_of_missing_details_1():
-    """Tests that the functions returns a list of missing keys when 
-    country and city keys are missing from plant data."""
-
-    data = {"origin_location": {"latitude": "", "longitude": ""}}
-    assert check_missing_location_details(data) == ["country", "city"]
+    data = {"temperature": "/",
+            "origin_location": "/", "botanist": "/", "last_watered": "", "soil_moisture": "",
+            "recording_taken": ""}
+    assert check_missing_keys(
+        data) == {"missing_keys": ["plant_id", "name"],
+                  "missing_values": ["last_watered", "soil_moisture", "recording_taken"]}
 
 
-def test_check_missing_location_details_return_list_of_missing_details_2():
-    """Tests that the functions returns a full list of missing keys when 
-    all location keys are missing from plant data."""
+def test_check_missing_keys_returns_empty_dict_if_no_missing_key_and_values():
+    """Tests that the function returns an empty dict if no keys and values are missing."""
 
-    data = {"origin_location": {}}
+    data = {"plant_id": "/", "name": "/", "temperature": "/",
+            "origin_location": "/", "botanist": "/", "last_watered": "/", "soil_moisture": "/",
+            "recording_taken": "/"}
+    assert check_missing_keys(
+        data) == {}
+
+
+def test_check_missing_location_details_returns_dict_of_missing_keys():
+    """Tests that the function returns a dict of missing keys 
+    if city and country keys are missing from plant data. """
+
+    data = {"origin_location": {"latitude": 7, "longitude": "/"}}
     assert check_missing_location_details(
-        data) == ["latitude", "longitude", "country", "city"]
+        data) == {"missing_keys": ["country", "city"]}
 
 
-def test_check_missing_location_details_return_empty_list_when_no_missing_details():
-    """Tests that the functions returns an empty list of missing keys when 
-    no botanist keys are missing from plant data."""
+def test_check_missing_location_details_returns_dict_of_missing_values():
+    """Tests that the function returns a dict of missing values 
+    if city and country keys have empty values."""
 
-    data = {"origin_location": {"latitude": "",
-                                "longitude": "", "country": "", "city": ""}}
+    data = {"origin_location": {"latitude": 7,
+                                "longitude": "/", "city": "", "country": ""}}
     assert check_missing_location_details(
-        data) == []
+        data) == {"missing_values": ["country", "city"]}
+
+
+def test_check_missing_location_details_returns_dict_of_missing_keys_and_values():
+    """Tests that the function returns a dict of missing keys and values if 
+    latitude key is missing and if city and country keys have empty values."""
+
+    data = {"origin_location": {"longitude": "/", "city": "", "country": ""}}
+    assert check_missing_location_details(
+        data) == {"missing_keys": ["latitude"], "missing_values": ["country", "city"]}
--- pipeline/validate.py
+++ pipeline/validate.py
@@ -13,17 +13,35 @@ def check_status_code(res):
         raise ValueError("Loading plant data was not successful.")
 
 
+def get_dict_of_missing_info(plant_dict, keys):
+
+    missing_info = {}
+    missing_keys = []
+    missing_values = []
+    for key in keys:
+        if key not in plant_dict:
+            missing_keys.append(key)
+        else:
+            if not plant_dict[key]:
+                missing_values.append(key)
+
+    if missing_keys:
+        missing_info["missing_keys"] = missing_keys
+    if missing_values:
+        missing_info["missing_values"] = missing_values
+
+    return missing_info
+
+
 def check_missing_keys(data):
     """Check that the plant data has all valid keys."""
 
     required_keys = ["plant_id", "name", "temperature", "origin_location", "botanist",
                      "last_watered", "soil_moisture", "recording_taken"]
 
-    missing_key = []
-    for key in required_keys:
-        if key not in data:
-            missing_key.append(key)
-    return missing_key
+    missing_info = get_dict_of_missing_info(data, required_keys)
+
+    return missing_info
 
 
 def check_missing_location_details(data):
@@ -31,11 +49,8 @@ def check_missing_location_details(data):
     location_details = data["origin_location"]
 
     required_keys = ["latitude", "longitude", "country", "city"]
-    missing_key = []
-    for key in required_keys:
-        if key not in location_details:
-            missing_key.append(key)
-    return missing_key
+    missing_info = get_dict_of_missing_info(location_details, required_keys)
+    return missing_info
 
 
 def check_missing_botanist_details(data):
@@ -43,26 +58,54 @@ def check_missing_botanist_details(data):
     botanist_details = data["botanist"]
 
     required_keys = ["name", "email", "phone"]
-    missing_key = []
-    for key in required_keys:
-        if key not in botanist_details:
-            missing_key.append(key)
-    return missing_key
+    missing_info = get_dict_of_missing_info(botanist_details, required_keys)
+    return missing_info
 
 
-def validate_data(data):
-    """Cleans and validates the plant data."""
+def validate_plant_data(data):
+    """Checks that relevant keys are not missing from the plant data."""
+    all_missing_keys = []
 
     missing_keys = check_missing_keys(data)
-    if missing_keys:
-        return f"Plant data has the following keys missing: {missing_keys}"
 
-    missing_details = {}
-    missing_details["location"] = check_missing_location_details(data)
-    missing_details["botanist"] = check_missing_botanist_details(data)
-    # missing_location = check_missing_location_details(data)
-    # missing_botanist = check_missing_botanist_details(data)
-    for key in missing_details:
-        if key:
-            return f"Mssing details for {key}: {missing_details[key]}"
+    if missing_keys:
+        all_missing_keys.append(
+            f"Plant data is missing the following: {missing_keys}")
+        return all_missing_keys
+
+    missing_location_details = check_missing_location_details(data)
+    missing_botanist_details = check_missing_botanist_details(data)
+
+    if missing_location_details:
+        all_missing_keys.append(
+            f"Missing keys for location: {missing_location_details}")
+    if missing_botanist_details:
+        all_missing_keys.append(
+            f"Missing keys for botanist: {missing_botanist_details}")
+
+    if all_missing_keys:
+        return all_missing_keys
     return data
+
+
+data = {
+
+    "name": "b",
+
+    "origin_location": {
+            "latitude": 43.74,
+            "longitude": -11.5098,
+            "country": ""
+    },
+    "botanist": {
+        "name": "",
+        "email": "",
+    },
+    "last_watered": "",
+    "soil_moisture": "",
+    "recording_taken": "hh"
+}
+
+m = validate_data_keys(data)
+
+print(m)
