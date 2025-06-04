fd0729e90871c37f1b7b39eac3eb988a4154d0c4
MariosT4 <micmartim@gmail.com>
2025-06-04 16:59:51 +0100

    Integrate with lambda handler, add to dockerfile

--- pipeline/pipeline_lambda.py
+++ pipeline/pipeline_lambda.py
@@ -1,24 +1,35 @@
 """Script that runs the pipeline on a lambda function."""
 
-# TODO: Remember to add any imports.
-from os import environ as ENV
-
 from dotenv import load_dotenv
+
+from extract import add_logger, retrieve_all_data, load_to_json
+from load import read_json_data, get_db_connection, load_origin_data, load_country_data, load_plant_master_data, load_sensor_reading_data
 
 
 def lambda_handler(event: dict, context: dict) -> dict:
     """Makes a lambda handler."""
 
-    # Pull data from API
-
-    # Transform data
+    # Extract data
+    file_logger = add_logger()
+    plant_data = retrieve_all_data(file_logger)
+    load_to_json(plant_data)
 
     # Load data
+    load_dotenv()
+
+    seed_data = read_json_data("plant_data.json")
+
+    conn = get_db_connection()
+
+    load_country_data(conn, seed_data)
+    load_origin_data(conn, seed_data)
+    load_plant_master_data(conn, seed_data)
+    load_sensor_reading_data(conn, seed_data)
+
+    conn.close()
 
     return {None: None}
 
 
 if __name__ == "__main__":
-    load_dotenv()
-
-    lambda_handler()
+    print(lambda_handler(None, None))
