834d273a7bf4c31821865d5a06be52b82cebad83
JamezDon <jamesdono70@btopenworld.com>
2025-06-06 16:24:22 +0100

    Remove unused imports

--- old_data/metadata_to_s3/metadata_pipeline_lambda.py
+++ old_data/metadata_to_s3/metadata_pipeline_lambda.py
@@ -1,8 +1,8 @@
 """ETL pipeline to upload all metadata to s3."""
 from dotenv import load_dotenv
 
-from extract import get_connection, get_time_range, make_query, store_data, get_metadata
-from transform import create_directories, get_unique, load_metadata
+from extract import get_metadata
+from transform import create_directories, load_metadata
 from load import connect_to_s3, load_files_to_bucket
 
 
